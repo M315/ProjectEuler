cbcc850ecb60bf5be084d5d5effa5abbf779b786
m315 <marc.jovanibertran@gmail.com>
2020-05-07 15:55:18 +0200

    jeje

--- 75/75.c
+++ 75/75.c
@@ -2,27 +2,50 @@
 #include <stdlib.h>
 #include <math.h>
 
+int gcd(int, int);
+
 int main(void){
-	int *list, i, a, b, c, N = 150000, count;
+	int *list, m, n, N = 1500000, limit, count, a, b, c, p;
 
 	list = (int*)calloc(N + 1, sizeof(int));
 
-	for(a = 3; a < N/3; a++){
-		for(b = a + 1; b < N - a; b++){
-			c = sqrt(a*a + b*b);
-			if(c*c == a*a + b*b && a+b+c <= N)
-				list[a+b+c]++;
+	limit = (int)sqrt(N/2);
+	count = 0;
+	for(m = 2; m <= limit ; m++){
+		for(n = 1; n < m; n++){
+			if((m + n) % 2 == 1 && gcd(m, n) == 1){
+				a = m*m - n*n;
+				b = 2*m*n;
+				c = m*m + n*n;
+				
+				p = a + b + c;
+				while(p < N){
+					list[p]++;
+					if(list[p] == 1)
+						count++;
+					if(list[p] == 2)
+						count--;
+					p += a + b + c;
+				}
+			}
 		}
 	}
 
-	count = 0;
-	for(i = 0; i <= N; i++)
-		if(list[i] == 1)
-			count++;
-	
 	printf("%d\n", count);
 
 	free(list);
 
 	return 0;
+}
+
+int gcd(int a, int b){
+	int t;
+
+	while(b != 0){
+		t = b;
+		b = a % b;
+		a = t;
+	}
+	
+	return a;
 }
