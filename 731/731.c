1a7b5753dcd78b0463d749047282dbd785840d16
m315 <marc.jovanibertran@gmail.com>
2020-10-27 10:45:52 +0100

    731 lets go

---
+++ 731/731.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sum(int[], int[]);
+
+int main(void){
+	int digits[200], num[200], i;
+
+	for(i = 0; i < 200; i++)
+		digits[i] = 3;
+
+	for(i = 0; i < 200; i++)
+		num[i] = 3; 
+
+
+}
+
+void sum(int a[], int b[]){
+	int i;
+
+	for(i = 0; i < 200; 
