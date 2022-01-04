87fa28f33382bc8979345142fad8a61435e93bae
M315 <m315@pop-os.localdomain>
2022-01-04 14:56:58 +0100

    86 done

---
+++ problem_86/target/debug/deps/problem_86-2ddc4434d1c62fa7.d
@@ -0,0 +1,5 @@
+/home/m315/Documents/code/ProjectEuler/problem_86/target/debug/deps/problem_86-2ddc4434d1c62fa7: src/main.rs
+
+/home/m315/Documents/code/ProjectEuler/problem_86/target/debug/deps/problem_86-2ddc4434d1c62fa7.d: src/main.rs
+
+src/main.rs:
---
+++ problem_86/target/debug/problem_86.d
@@ -0,0 +1,1 @@
+/home/m315/Documents/code/ProjectEuler/problem_86/target/debug/problem_86: /home/m315/Documents/code/ProjectEuler/problem_86/src/main.rs
