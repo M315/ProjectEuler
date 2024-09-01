99972093b36671f3945883c245b68e1fef62af57
M315 <marc.jovanibertran@gmail.com>
2024-09-01 15:39:14 +0200

    Ups! Wrong code

--- problem_91/src/main.rs
+++ problem_91/src/main.rs
@@ -1,99 +1,48 @@
-struct Triplet(u64, u64, u64);
-
-// Generate pitagorean triplets 
-fn pythagorean_triplets(k : u64) -> Vec<Triplet>
-{
-    let mut ret : Vec<Triplet> = Vec::new();
-
-    let mut n : u64 = 1;
-    let mut m : u64 = 2;
-
-    while m * m + n * n < 2 * k
-    {
-        if fast_gcd(n, m) == 1 && n % 2 != m % 2
-        {
-            let t : Triplet = Triplet(m * m - n * n, 2 * m * n, m * m + n * n);
-            ret.push(t);
-        }
-
-        n += 1;
-        if n == m
-        {
-            n = 1;
-            m += 1;
-        }
-    }
-
-    return ret;
+struct OriginTriangle {
+    p: (i32, i32),
+    q: (i32, i32),
 }
 
-// Fast iterative version of Euclid's GCD algorithm
-fn fast_gcd(mut a: u64, mut b: u64) -> u64 {
-    while b != 0 {
-        a %= b;
-        std::mem::swap(&mut a, &mut b);
-    }
-    return a;
-}
-
-// Given a and b+c retruns how many rutes there are smaller than m
-fn count_rutes(a : u64, x : u64, m : u64) -> u64
-{
-    if a > m
-    {
-        return 0;
-    }
-
-    if x <= a
-    {
-        return x / 2;
-    }
-    else
-    {
-        if 2 * a >= x
-        {
-            return a - (x + 1) / 2 + 1;
-        }
-        else
-        {
-            return 0;
+impl OriginTriangle {
+    fn is_right_triangle(self) -> bool {
+        let a2 = self.p.0 * self.p.0 + self.p.1 * self.p.1;
+        let b2 = self.q.0 * self.q.0 + self.q.1 * self.q.1;
+        let c2 = (self.p.0 - self.q.0) * (self.p.0 - self.q.0) + (self.p.1 - self.q.1) * (self.p.1 - self.q.1);
+        if a2 == 0 || b2 == 0 || c2 == 0 { return false; }
+        if a2 + b2 == c2 || a2 + c2 == b2 || b2 + c2 == a2 {
+            true
+        } else {
+            false
         }
     }
 }
 
-fn solve(m : u64, triplets : &Vec<Triplet>) -> u64
+fn solve(n: usize) -> usize
 {
-    let mut count : u64 = 0;
-    for Triplet(x, y, _) in triplets
-    {
-        let mut k = 1;
-        while x * k <= m || y * k <= m
-        {
-            count += count_rutes(x * k, y * k, m);
-            count += count_rutes(y * k, x * k, m);
-            k += 1;
+    let mut seen: Vec<Vec<Vec<Vec<bool>>>> = vec![vec![vec![vec![false; n + 1]; n + 1]; n + 1]; n + 1];
+    let mut ans: usize = 0;
+    for i in 0..=n {
+        for j in 0..=n {
+            for r in 0..=n {
+                for s in 0..=n {
+                    if seen[i][j][r][s] || seen[r][s][i][j] {
+                        continue;
+                    }
+                    seen[i][j][r][s] = true;
+                    seen[r][s][i][j] = true;
+
+                    let triangle = OriginTriangle{ p: (i as i32, j as i32), q: (r as i32, s as i32) };
+                    if triangle.is_right_triangle() {
+                        ans += 1;
+                    }
+                }
+            }
         }
     }
-
-    return count;
+    
+    ans
 }
 
-fn main()
-{
-    let m : u64 = 1_000_000;
-    let k : u64 = (m as f64).log(10.) as u64;
-    let triplets : Vec<Triplet> = pythagorean_triplets(k * m);
-    println!("{}", solve(m, &triplets));
-    /*
-    let mut m : u64 = 1800;
-    let k : u64 = (m as f64).log(10.) as u64;
-    let triplets : Vec<Triplet> = pythagorean_triplets(k * m);
-
-    while solve(m, &triplets) < 1_000_000
-    {
-        m += 1;
-    }
-
-    println!("{}", m);
-    */
+fn main() {
+    println!("{}", solve(50));
 }
